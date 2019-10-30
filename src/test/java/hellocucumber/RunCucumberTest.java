93fb7107101b57e4f33fdc843383635bce1321ec
Yuliia Horenko <yuliia_horenko@epam.com>
2019-10-30 16:37:22 +0200

    configured runner class

--- src/test/java/hellocucumber/RunCucumberTest.java
+++ src/test/java/hellocucumber/RunCucumberTest.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = "pretty",
         features = "src/test/resources/features",
-        glue = "src/test/java/hellocucumber/"
+        glue = "hellocucumber"
 )
 
 public class RunCucumberTest {
