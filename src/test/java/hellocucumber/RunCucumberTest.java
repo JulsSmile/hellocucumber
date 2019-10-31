a8008315176ecbc9e678a42e9765b641e9472885
Yuliia Horenko <yuliia_horenko@epam.com>
2019-10-31 16:01:25 +0200

    pom.fix

--- src/test/java/hellocucumber/RunCucumberTest.java
+++ src/test/java/hellocucumber/RunCucumberTest.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = "pretty",
         features = "src/test/resources/features",
-        glue = {"hellocucumber"}
+        glue = {"RunCucumberTest.java"}
 )
 
 public class RunCucumberTest{
